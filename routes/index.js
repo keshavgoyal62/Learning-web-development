db9ec27a7049154bdf5b18f50b2fe992b757d5e8
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-16 23:48:21 +0530

    Add more controller and router for home

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const homeController=require('../controllers/home_controllers');
 console.log('router loaded')
 
 router.get('/',homeController.home);
+router.use('/user',require('./user'));
+
+//for any further routes acess from here
+//router.use('/routerName',require('./routerfile'))
 
 module.exports = router;
 

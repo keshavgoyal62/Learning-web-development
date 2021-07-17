4fa9b33115a622241a3041ccaf13119272783a6d
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-17 13:18:54 +0530

    adding controller and routes for post

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ console.log('router loaded')
 
 router.get('/',homeController.home);
 router.use('/user',require('./user'));
-
+router.use('/post',require('./post'));
 //for any further routes acess from here
 //router.use('/routerName',require('./routerfile'))
 

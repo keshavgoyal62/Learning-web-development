95e8dd6cff2dcc782530a928302ec678f62a5180
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-18 20:17:02 +0530

    creating the form for sign in and sign up pages just before cookies

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -3,3 +3,28 @@ module.exports.profile =function(req,res){
         title: "User Profile"
     });
 }
+
+
+//render sign up page
+module.exports.signUp= function(req,res){
+    return res.render('user_sign_up',{
+        title : "Codeial | Sign up" 
+    });
+}
+
+//render sign in page
+module.exports.signIn= function(req,res){
+    return res.render('user_sign_in',{
+        title : "Codeial | Sign in" 
+    });
+}
+
+//get the sign up data
+module.exports.create=function(req,res){
+    //TODO LATER
+}
+
+//create the sign in session
+module.exports.create=function(req,res){
+    //TODO LATER
+}
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,8 @@ const userController = require('../controllers/users_controllers');
 
 router.get('/profile',userController.profile);
 
+router.get('/sign-up',userController.signUp);
+
+router.get('/sign-in',userController.signIn);
+
 module.exports = router;

c8a83668a3862b5f9a5a391669e3f5b42d51cd18
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-20 22:05:24 +0530

    Completed showing the comments on home page

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,3 +1,4 @@
+const { populate } = require('../models/post');
 const Post = require('../models/post');
 
 module.exports.home =function(req,res){
@@ -11,7 +12,15 @@ module.exports.home =function(req,res){
     // })
 
     //populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title: "Home",
             posts: posts

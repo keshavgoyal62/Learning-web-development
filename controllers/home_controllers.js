19b0fda0900f0ff4c7549f0c21136b1f4c2cbec0
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-23 21:56:12 +0530

    Async in home controller

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -2,35 +2,28 @@ const { populate } = require('../models/post');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home =function(req,res){
-    //res.cookie('user_id',25);
-
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title: "Home",
-    //         posts: posts
-    //     });
-    // })
-
+module.exports.home = async function(req,res){
     //populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "Home",
-                posts: posts,
-                all_users : users
-            });
-        })
-        
-    })
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+        let users = await User.find({});
+        return res.render('home',{
+            title: "Home",
+            posts: posts,
+            all_users : users
+        });
+    }
+    catch(err){
+        console.log(err);
+        return ;
+    }
 }
 
 // module.exports.actionName = funtion(req,res){} 

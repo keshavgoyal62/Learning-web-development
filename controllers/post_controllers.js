924015220076a548938da47c41e2e0ca584c351a
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-26 21:03:52 +0530

    AJAX HANDLED CREATING AND DELETING POST

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -3,10 +3,21 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post: post,
+
+                },
+                message: "Post created!"
+            });
+        }
+
         req.flash('success','Post published!');
         return res.redirect('back');
     }
@@ -23,6 +34,17 @@ module.exports.destroy = async function(req,res){
             if(post.user==req.user.id){
                 post.remove();
                 await Comment.deleteMany({post: req.params.id});
+
+                if(req.xhr){
+                    return res.status(200).json({
+                        data:{
+                            post_id: req.params.id,
+        
+                        },
+                        message: "Post deleted!"
+                    });
+                }
+
                 req.flash('success','Post Deleted!');
                 return res.redirect('back');
             }

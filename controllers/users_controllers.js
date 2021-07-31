af1eb30860cf1c7e3dacf2dbfe23ee441fc8e033
KeshavGoyal <goyalkeshav999@gmail.com>
2021-07-31 20:08:55 +0530

    ADDED MULTER

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,4 +1,7 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports.profile =function(req,res){
     User.findById(req.params.id,function(err,user){
@@ -10,13 +13,42 @@ module.exports.profile =function(req,res){
     
 }
 
-module.exports.update = function(req,res){
-    if(req.isAuthenticated()){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+module.exports.update = async function(req,res){
+    // if(req.isAuthenticated()){
+    //     User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+    //         return res.redirect('back');
+    //     })
+    // }
+    // else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+    if(req.user.id==req.params.id){
+        try{
+
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err){
+                    console.log('*********MULTER ERROR*********',err);
+                }
+                console.log(req.file);
+                user.name=req.body.name;
+                user.email=req.body.name;
+                if(req.file){
+                    if(fs.existsSync(path.join(__dirname,'..',user.avatar)))
+                        fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                }
+                user.avatar=User.avatarPath+'/'+req.file.filename;
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error',err);
             return res.redirect('back');
-        })
+        }
     }
     else{
+        req.flash('error','Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
 }
